73ecc63ecb0e1358fd561f3e6ef5b810a9311fb9
Abey Onalaja <abey.onalaja@gmail.com>
2016-04-08 19:33:27 +0100

    Move all players

--- index.js
+++ index.js
@@ -3,22 +3,23 @@ var io = require('socket.io')(process.env.PORT || 3000);
 var shortid = require('shortid');
 
 
-var playerCount = 0;
+var players = [];
 console.log('server started')
 
 io.on('connection', function(socket){
 
     var thisClientid = shortid.generate();
+    players.push(thisClientid);
     console.log('client connected, broadcasting spawn ' + thisClientid);
 
     socket.broadcast.emit('spawn', { id: thisClientid });
 
-    playerCount++;
-
-    for(i=0; i< playerCount; i++) {
-        socket.emit('spawn');
+    players.forEach(function(playerId){
+        if( playerId == thisClientid )
+            return;
+        socket.emit('spawn', { id: playerId}); 
         console.log('sending spawn to new player');
-    }
+    });
 
     socket.on('move', function(data){
         data.id = thisClientid;
